0ffccdb4bc507679885f8b3934ef83b05640a1d4
Avazbek Isroilov <64166521+Avazbek2002@users.noreply.github.com>
2022-08-14 16:22:00 +0500

    hello

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,44 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal (String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public Animal () {}
+
+    public void getDescription() {
+        System.out.println("This animal is mostly " + this.color + ". It has " +
+                ((this.numberOfPaws == 1)?(" paw "):(this.numberOfPaws + " paws ")) + " and " + (this.hasFur?"a":"no") + " fur");
+    }
+
+    public void setColor (String color) {
+        this.color = color;
+    }
+
+    public void setNumberOfPaws (int numberOfPaws) {
+        this.numberOfPaws = numberOfPaws;
+    }
+
+    public void setHasFur (boolean hasFur) {
+        this.hasFur = hasFur;
+    }
+
+    public String getColor () {
+        return this.color;
+    }
+
+    public int getNumberOfPaws () {
+        return this.numberOfPaws;
+    }
+
+    public boolean getHasFur () {
+        return this.hasFur;
+    }
 }
